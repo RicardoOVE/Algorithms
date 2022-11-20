c2a02df7cba41500fb798248a2c30faae5ac9935
Ricardo Ortiz <75132128+RicardoOVE@users.noreply.github.com>
2022-11-20 17:25:40 -0500

    General changes

---
+++ ListaEncadenadaJs/ListaEncadenada.js
@@ -0,0 +1,10 @@
+const Nodo = require('./Nodo')
+
+module.exports = class ListaEncadenada {
+    constructor() {
+        this.head = null;
+    }
+}
+
+// Inserta Nodo:
+
---
+++ ListaEncadenadaJs/Nodo.js
@@ -0,0 +1,7 @@
+module.exports = class Nodo {
+    constructor(nombre, id) {
+        this.nombre = nombre;
+        this.id = id;
+        this.next = null;
+    }
+}
